8671d32613894b5af110f0820be488f0c520bb64
Gabe Montero <gmontero@redhat.com>
2023-03-09 16:58:22 -0500

    add openshift route type to scheme for registration service proxy plugin support (#303)

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -6,6 +6,8 @@ import (
 	authenticationv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	routev1 "github.com/openshift/api/route/v1"
 )
 
 // AddToScheme adds all required resources to the default Scheme
@@ -15,6 +17,7 @@ func AddToScheme(s *runtime.Scheme) error {
 		toolchainv1alpha1.AddToScheme,
 		corev1.AddToScheme,
 		authenticationv1.AddToScheme, // used by the registration service proxy to verify cached tokens
+		routev1.Install,              // used by the registration service to access proxy plugin endpoints exposed via openshift routes
 	)
 	return addToSchemes.AddToScheme(s)
 }
