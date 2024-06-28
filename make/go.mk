7f226f55a34a82f4b4a9b4f95684dfefad64674e
Feny Mehta <fbm3307@gmail.com>
2024-06-28 10:38:07 +0530

    KUBESAW-124: Makefile target for dependency validations (#410)
    * KUBESAW-124: Makefile target for dependency validations
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    * adding build
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    
    ---------
    
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>
    Co-authored-by: Francisc Munteanu <fmuntean@redhat.com>

--- make/go.mk
+++ make/go.mk
@@ -16,3 +16,15 @@ build: $(shell find . -path ./vendor -prune -o -name '*.go' -print)
 .PHONY: vendor
 vendor:
 	$(Q)go mod vendor
+
+.PHONY: verify-dependencies
+## Runs commands to verify after the updated dependecies of toolchain-common/API(go mod replace), if the repo needs any changes to be made
+verify-dependencies: tidy vet build test lint-go-code
+
+.PHONY: tidy
+tidy: 
+	go mod tidy
+
+.PHONY: vet
+vet:
+	go vet ./...
