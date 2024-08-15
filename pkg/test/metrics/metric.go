a209f6c3549dd4217a59088e267598d519a58bc5
Kanika Rana <46766610+ranakan19@users.noreply.github.com>
2024-08-15 10:45:50 -0400

    move wrapper for metric float comparision to common (#423)

---
+++ pkg/test/metrics/metric.go
@@ -0,0 +1,17 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	promtestutil "github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func AssertMetricsCounterEquals(t *testing.T, expected int, c prometheus.Counter) {
+	assert.InDelta(t, float64(expected), promtestutil.ToFloat64(c), 0.01)
+}
+
+func AssertMetricsGaugeEquals(t *testing.T, expected int, g prometheus.Gauge, msgAndArgs ...interface{}) {
+	assert.InDelta(t, float64(expected), promtestutil.ToFloat64(g), 0.01, msgAndArgs...)
+}
