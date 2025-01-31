a9c24d874b32f6d857d79e5f54ced0d68cd1ad97
Kanika Rana <46766610+ranakan19@users.noreply.github.com>
2025-01-31 17:37:55 -0500

    Upgrade go and k8s depencencies for Openshift 4.16 (#449)
    * update versions wrt openshift 4.16
    
    * update operator-utils
    
    * mod tidy after conflict resolve
    
    * remove deprecated pointer, replace with ptr
    
    * revert uatest package name
    
    * tidy
    
    * remove pointer deprecated
    
    * update api replace, and api version
    
    * fix version in readme
    
    * remove replace block
    
    ---------
    
    Co-authored-by: Feny Mehta <fbm3307@gmail.com>

--- pkg/client/rest_client.go
+++ pkg/client/rest_client.go
@@ -3,11 +3,10 @@ package client
 import (
 	"context"
 	"fmt"
-
 	authv1 "k8s.io/api/authentication/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/rest"
-	"k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 )
 
 // CreateTokenRequest creates a TokenRequest for a service account using given expiration in seconds.
@@ -15,7 +14,7 @@ import (
 func CreateTokenRequest(ctx context.Context, restClient *rest.RESTClient, namespacedName types.NamespacedName, expirationInSeconds int) (string, error) {
 	tokenRequest := &authv1.TokenRequest{
 		Spec: authv1.TokenRequestSpec{
-			ExpirationSeconds: pointer.Int64(int64(expirationInSeconds)),
+			ExpirationSeconds: ptr.To(int64(expirationInSeconds)),
 		},
 	}
 	result := &authv1.TokenRequest{}
--- pkg/configuration/cache_test.go
+++ pkg/configuration/cache_test.go
@@ -17,7 +17,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 )
 
 func TestCache(t *testing.T) {
@@ -318,7 +318,7 @@ func TestMultipleExecutionsInParallel(t *testing.T) {
 	latch.Add(1)
 	var waitForFinished sync.WaitGroup
 	initconfig := NewToolchainConfigObjWithReset(t, testconfig.Members().SpecificPerMemberCluster("member", toolchainv1alpha1.MemberOperatorConfigSpec{
-		Environment: pointer.String("env"),
+		Environment: ptr.To("env"),
 	}))
 
 	secret := &v1.Secret{
@@ -353,7 +353,7 @@ func TestMultipleExecutionsInParallel(t *testing.T) {
 			defer waitForFinished.Done()
 			latch.Wait()
 			config := NewToolchainConfigObjWithReset(t, testconfig.Members().SpecificPerMemberCluster(fmt.Sprintf("member%d", i), toolchainv1alpha1.MemberOperatorConfigSpec{
-				Environment: pointer.String(fmt.Sprintf("env%d", i)),
+				Environment: ptr.To(fmt.Sprintf("env%d", i)),
 			}))
 
 			secretData := map[string]map[string]string{
