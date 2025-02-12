7e89acb4a66b0c521b3b5298d595d09e60520e2b
Xavier Coulon <xcoulon@redhat.com>
2025-02-12 09:40:15 +0100

    test: metric assertions and comparisons (#453)
    new helper funcs
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/test/metrics/metric.go
+++ pkg/test/metrics/metric.go
@@ -12,6 +12,14 @@ func AssertMetricsCounterEquals(t *testing.T, expected int, c prometheus.Counter
 	assert.InDelta(t, float64(expected), promtestutil.ToFloat64(c), 0.01)
 }
 
+func AssertCounterEqualsInt(t *testing.T, expected int, c prometheus.Counter) {
+	assert.Equal(t, expected, int(promtestutil.ToFloat64(c)))
+}
+
+func AssertCounterGreaterOrEqualsInt(t *testing.T, threshold int, c prometheus.Counter) {
+	assert.GreaterOrEqual(t, int(promtestutil.ToFloat64(c)), threshold)
+}
+
 func AssertMetricsGaugeEquals(t *testing.T, expected int, g prometheus.Gauge, msgAndArgs ...interface{}) {
 	assert.InDelta(t, float64(expected), promtestutil.ToFloat64(g), 0.01, msgAndArgs...)
 }
