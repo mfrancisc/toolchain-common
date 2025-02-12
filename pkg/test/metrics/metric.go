e5452d8cf9a4090d155f5485b55155043cb05db7
Xavier Coulon <xcoulon@redhat.com>
2025-02-12 13:43:16 +0100

    test: metric helper func (#455)
    new helper func to obtain a Prometheus counter value
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/test/metrics/metric.go
+++ pkg/test/metrics/metric.go
@@ -13,11 +13,15 @@ func AssertMetricsCounterEquals(t *testing.T, expected int, c prometheus.Counter
 }
 
 func AssertCounterEqualsInt(t *testing.T, expected int, c prometheus.Counter) {
-	assert.Equal(t, expected, int(promtestutil.ToFloat64(c)))
+	assert.Equal(t, expected, GetCounterInt(c))
+}
+
+func GetCounterInt(c prometheus.Counter) int {
+	return int(promtestutil.ToFloat64(c))
 }
 
 func AssertCounterGreaterOrEqualsInt(t *testing.T, threshold int, c prometheus.Counter) {
-	assert.GreaterOrEqual(t, int(promtestutil.ToFloat64(c)), threshold)
+	assert.GreaterOrEqual(t, GetCounterInt(c), threshold)
 }
 
 func AssertMetricsGaugeEquals(t *testing.T, expected int, g prometheus.Gauge, msgAndArgs ...interface{}) {
